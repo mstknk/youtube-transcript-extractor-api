1458b2606d60aaba59c005248496c21ee757cb00
mstknk <mesutkonak89@gmail.com>
2023-03-03 10:20:04 +0100

    remove storing mongo

--- _settings.py
+++ _settings.py
@@ -1,11 +1,1 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-WATCH_URL = os.environ.get("WATCH_URL")
-MONGO_DB = os.environ.get("MONGO_DB")
-MONGO_DB_USERNAME = os.environ.get("MONGO_DB_USERNAME")
-MONGO_DB_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
-MONGO_HOST = os.environ.get("MONGO_HOST")
-MONGO_TRANSCRIPT_COLLECTION = os.environ.get("MONGO_TRANSCRIPT_COLLECTION")
-MONGO_YOUTUBE_COLLECTION = os.environ.get("MONGO_YOUTUBE_COLLECTION")
+WATCH_URL = 'https://www.youtube.com/watch?v={video_id}'
--- app.py
+++ app.py
@@ -1,43 +1,23 @@
 import logging
-import pymongo
 import json
-from _settings import MONGO_DB_USERNAME
-from _settings import MONGO_DB_PASSWORD
-from _settings import MONGO_HOST
-from _settings import MONGO_DB
-from _settings import MONGO_TRANSCRIPT_COLLECTION
-from _settings import MONGO_YOUTUBE_COLLECTION
 from flask import Flask
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
 logger = logging.getLogger(__name__)
-myClient = pymongo.MongoClient('mongodb://%s:%s@%s' % (MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_HOST))
-mydb = myClient[MONGO_DB]
-transcript_collection = mydb[MONGO_TRANSCRIPT_COLLECTION]
-youtube_collection = mydb[MONGO_YOUTUBE_COLLECTION]
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/api/transcripts/<string:youtube_id>', methods=['POST'])
-def import_youtube_transcript_api(youtube_id):
-    logger.info("importing youtube transcript for  %s", youtube_id)
+def youtube_transcript_api(youtube_id):
+    logger.info("getting transcript for youtubeId %s", youtube_id)
     try:
-        if youtube_collection.count_documents({'youtubeId': youtube_id}) > 0:
-            logger.info("transcript already stored :  %s", youtube_id)
-        else:
-            transcript = YouTubeTranscriptApi.get_transcript(youtube_id)
-            data = {'transcript': transcript, 'youtubeId': youtube_id}
-            youtube_collection.insert_one(data)
-            result = [{**item, 'youtubeId': youtube_id} for item in transcript]
-
-            transcript_collection.insert_many(result)
-            logger.info("transcript stored : %s", youtube_id)
-
+        transcript = YouTubeTranscriptApi.get_transcripts([youtube_id], languages=['de'])
     except Exception as exception:
         logger.error("Error: " + exception.cause)
         return json.dumps({"error": exception.cause}), 404
-    return json.dumps({"success": True}), 201
+    return json.dumps({"result": transcript}), 201
 
 
 if __name__ == '__main__':
